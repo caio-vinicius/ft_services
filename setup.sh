2441f968ac8f81df7313cb9eee8163940f11a769
caio-vinicius <contato@caiovinicius.dev.br>
2020-08-15 09:57:01 -0300

    incrementing setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 minikube start
-minikube addons enabled metallb
+minikube addons enable metallb
 eval $(minikube docker-env)
 kubectl apply -f configmap_metallb.yaml
-kubectl apply -f nginx.yaml
+cd nginx && \
+    docker build -t nginx:services . && kubectl apply -f nginx.yaml && cd ..
+cd ftp && \
+    docker build -t ftp:services . && kubectl apply -f ftp.yaml && cd ..
+cd wordpress && \
+    docker build -t wordpress:services . && kubectl apply -f wordpress.yaml && \
+    cd mariadb && docker build -t mariadb:services . && kubectl apply -f mariadb.yaml && cd ..
+cd phpmyadmin && docker build -t phpmyadmin:services . && kubectl apply -f phpmyadmin.yaml && \
+    cd .. && cd ..
+cd grafana && docker build -t grafana:services . && kubectl apply -f grafana.yaml && \
+    cd influxdb && docker build -t influxdb:services . && kubectl apply -f influxdb.yaml && cd .. && \
+    cd telegraf && docker build -t telegraf:services . && kubectl apply -f telegraf.yaml && \
+    cd .. && cd ..
+minikube dashboard
