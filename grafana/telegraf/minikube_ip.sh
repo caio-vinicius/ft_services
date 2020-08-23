b4375e2b7bcc389fb161aa029710e3b832fb45d9
user42 <contato@caiovinicius.dev.br>
2020-08-23 02:14:42 +0200

    fix bug to get ip on linux vm

--- grafana/telegraf/minikube_ip.sh
+++ grafana/telegraf/minikube_ip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export MINIKUBE_IP=$(minikube ip)
-sed -i '20s|*url =*|      url = "https://'$MINIKUBE_IP':10250"|g' ./telegraf.yaml
+export MINIKUBE_IP=$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)
+sed -i '20s|url = ""|url = "https://'$MINIKUBE_IP':10250"|g' ./telegraf.yaml
